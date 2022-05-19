10f27ed1596f63781633aeddd7d9025060672e57
AlexanderSanchezNL <66374860+AlexanderSanchezNL@users.noreply.github.com>
2022-05-19 19:37:22 +0200

    Alternative to set_index approach

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
 
 # Display table
 streamlit.dataframe(my_fruit_list)
