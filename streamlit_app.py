8d60fd0f96cb7643f1f4d1a2b9a08598de9c9f8c
AlexanderSanchezNL <66374860+AlexanderSanchezNL@users.noreply.github.com>
2022-05-19 19:42:54 +0200

    Issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-#my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Pick list
 # Choose a Few Fruits to Set a Good Example
 # We'll ask our app to put the list of selected fruits into a variable called fruits_selected. 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display table
